76f174eca9c9c71ab055b50f8245609545d93be9
CHEONKUK <dd1139001@naver.com>
2019-10-21 11:44:15 +0900

    191021
    # React Study
    -- CSS & StyleComponent

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,30 +2,48 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const List = styled.ul`
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
   display: flex;
-  &:hover {
-    background-color: red;
-  }
+  align-items: center;
+  background-color: rgba(20, 20, 20, 0.8);
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
 `;
 
-const Header = styled.header``;
+const List = styled.ul`
+  display: flex;
+`;
 
-const Item = styled.li``;
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+`;
 
-const SLink = styled(Link)``;
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default () => (
   <Header>
     <List>
       <Item>
-        <Link to="/">Moives</Link>
+        <SLink to="/">Moives</SLink>
       </Item>
       <Item>
-        <Link to="/tv">TV</Link>
+        <SLink to="/tv">TV</SLink>
       </Item>
       <Item>
-        <Link to="/search">Search</Link>
+        <SLink to="/search">Search</SLink>
       </Item>
     </List>
   </Header>
