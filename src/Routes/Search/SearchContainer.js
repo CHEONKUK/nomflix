267b4900b402c178ec492e854703451294d508f5
CHEONKUK <dd1139001@naver.com>
2019-10-25 13:19:54 +0900

    191025
    # React Study
    -- SearchContainer

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,13 +1,43 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
+import { movieApi } from "api";
+import { tvApi } from "../../api";
 
 export default class extends React.Component {
   state = {
     movieResult: null,
     tvResult: null,
-    searchTerm: "",
+    searchTerm: "code",
     error: null,
     loading: false
+  };
+
+  componentDidMount() {
+    this.handleSubmit();
+  }
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchTerm();
+    }
+  };
+
+  searchTerm = async () => {
+    const { searchTerm } = this.state;
+
+    try {
+      const movieResult = await movieApi.search(searchTerm);
+      const tvResult = await tvApi.search(searchTerm);
+
+      console.log(movieResult, tvResult);
+
+      this.setState({ loading: true });
+    } catch {
+      this.setState({ error: "Can't find results." });
+    } finally {
+      this.setState({ loading: false });
+    }
   };
 
   render() {
